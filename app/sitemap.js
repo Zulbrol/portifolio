d6a7ad7ed7624dd0afcdc2c2f8b6ca7deab27e30
Zulbrol <corteslucas432@gmail.com>
2026-07-08 22:48:20 -0300

    fix: corrige URL do sitemap com letra certa

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://vercel.app', // <-- Troque pelo link completo do seu site na Vercel
+      url: 'https://portifolio-liard-kappa.vercel.app', // <-- Ajustado com "i"
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
