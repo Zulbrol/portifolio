a1d2292e5963347f61432cea4310bf03fea89e71
Zulbrol <corteslucas432@gmail.com>
2026-07-10 21:09:24 -0300

    teste google

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://portifolio-liard-kappa.vercel.app', // <-- Ajustado com "i"
+      url: 'https://portifolio-liard-kappa.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
